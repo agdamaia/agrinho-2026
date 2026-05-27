1415a51ac5df0f2a0f4192339b71c1ae33b8b766
agdamaia <agda.maia@escola.pr.gov.br>
2026-05-27 20:29:05 -0300

    teste

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let pinhões = 0;
+const contadorElemento = document.getElementById('contador');
+const statusElemento = document.getElementById('status');
 
+function colher() {
+    pinhões++;
+    contadorElemento.innerText = pinhões;
+
+    // Lógica de feedback baseada na quantidade
+    if (pinhões > 0 && pinhões <= 10) {
+        statusElemento.innerText = "Início da safra!";
+        statusElemento.style.color = "#2d5a27";
+    } else if (pinhões > 10 && pinhões <= 20) {
+        statusElemento.innerText = "Produção a todo vapor!";
+        statusElemento.style.color = "#fbc02d";
+    } else if (pinhões > 20) {
+        statusElemento.innerText = "Cesto cheio! Ótima colheita!";
+        statusElemento.style.color = "#d84315";
+    }
+}
+
+function reiniciar() {
+    pinhões = 0;
+    contadorElemento.innerText = pinhões;
+    statusElemento.innerText = "Aguardando época (Abril)";
+    statusElemento.style.color = "#2d3436";
+}
